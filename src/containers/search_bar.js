63dca80962a37cdf6ac6caf79048769d89642e59
seanrtaylor <seantaylorguitars@gmail.com>
2017-06-08 14:28:24 -0700

    fix form default

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -14,9 +14,13 @@ export default class SearchBar extends Component {
     this.setState({term: event.target.value})
   }
 
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
   render () {
     return(
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           placeholder="Get a five-day forcast"
           className="form-control"
