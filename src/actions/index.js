1bb3fa0f2226f862892036ced9e3c38b266c8a5e
seanrtaylor <seantaylorguitars@gmail.com>
2017-06-08 16:56:44 -0700

    building table

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const API_KEY = '3b4330ed2cdaa954d0449b2b3bea1958';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
@@ -7,7 +9,10 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city){
   const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
   return {
-    type: 'FETCH_WEATHER'
+    type: 'FETCH_WEATHER',
+    payload: request
   };
 }
